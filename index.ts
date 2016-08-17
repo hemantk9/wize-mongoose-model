87bcca68c6c8c3766ccf84c3c8a497085cfef5f9
hemantpopli <hemant@wizni.com>
2016-08-17 13:10:32 +0530

    First commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { Connection } from "mongoose";
+import { WizeSchema } from "wize-schema";
+import { createSchema, createHistorySchema } from "wize-mongoose-schema";
+
+class WizeMongooseModel {
+    public model: any;
+    public historyModel: any;
+    constructor(public metadata: WizeSchema, connection: Connection) {
+        this.model =  connection.model(this.metadata.name, createSchema(this.metadata));
+        if(this.metadata.versioning) {
+            this.historyModel = connection.model(`history_${this.metadata.name}`, createHistorySchema(this.metadata.name));
+        }
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,12 @@
+"use strict";
+const wize_mongoose_schema_1 = require("wize-mongoose-schema");
+class WizeMongooseModel {
+    constructor(metadata, connection) {
+        this.metadata = metadata;
+        this.model = connection.model(this.metadata.name, wize_mongoose_schema_1.createSchema(this.metadata));
+        if (this.metadata.versioning) {
+            this.historyModel = connection.model(`history_${this.metadata.name}`, wize_mongoose_schema_1.createHistorySchema(this.metadata.name));
+        }
+    }
+}
+//# sourceMappingURL=index.js.map
