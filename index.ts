27c11b85f783baeaae664561afdf8aba91f407c9
hemantpopli <hemant@wizni.com>
2016-08-22 10:32:53 +0530

    minor changes

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import { Connection } from "mongoose";
+import { Connection, Model, Document } from "mongoose";
 import { WizeSchema } from "wize-schema";
 import { createSchema, createHistorySchema } from "wize-mongoose-schema";
 
 export class WizeMongooseModel {
-    public model: any;
-    public historyModel: any;
+    public model: Model<Document>;
+    public historyModel:  Model<Document>;;
     constructor(public metadata: WizeSchema, connection: Connection) {
         this.model =  connection.model(this.metadata.name, createSchema(this.metadata));
         if(this.metadata.versioning) {
